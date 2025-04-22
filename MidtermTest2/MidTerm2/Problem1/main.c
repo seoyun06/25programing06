7bada2b7b171660f077b59c52717d0598894c4ba
seoyun06 <a87111707@gmail.com>
2025-04-22 14:50:22 +0900

    Add files via upload

---
+++ MidtermTest2/MidTerm2/Problem1/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("    o    o    \n");
+    printf("    |    |    \n");
+
+    printf("   ---------  \n");
+    printf("   |VVVVVVVV| \n");
+    printf("   |        |  \n");
+    printf("   ---------\n");
+    printf(" --------------\n");
+    printf(" |VVVVVVVVVVVV|\n");
+    printf(" |            |\n");
+    printf(" --------------\n");
+    return 0;
+}
